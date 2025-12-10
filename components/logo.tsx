52ed4bd0ad1b4722481310322993f9a41c741a70
Ilesanmi Tolulope <marvellousilesanmi25@gmail.com>
2025-12-11 00:37:39 +0100

    made the logo clickable

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Sparkles } from "lucide-react";
+import Link from "next/link";
 
 const Logo = () => {
   return (
     <div className="flex items-center gap-2">
-      <div className="relative ">
-        {/* <div className="absolute inset-0 accent-bg rounded-lg blur-xl opacity-50" /> */}
-        <Sparkles className="relative h-8 w-8 accent-text" />
-      </div>
-      <span className="text-2xl font-bold gradient-text">Tourify</span>
+      <Link href="/" className="flex items-center gap-3">
+        <div className="relative ">
+          {/* <div className="absolute inset-0 accent-bg rounded-lg blur-xl opacity-50" /> */}
+          <Sparkles className="relative h-8 w-8 accent-text" />
+        </div>
+        <span className="text-2xl font-bold gradient-text">Tourify</span>
+      </Link>
     </div>
   );
 };
